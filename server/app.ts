2308805a3649cfee7b65561e51addbf2d312a74e
mya-ake <make.app.ts@gmail.com>
2019-07-28 11:13:33 +0900

    feat: Add helmet

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import helmet from 'helmet';
 import { nuxt } from './core/nuxt';
 import { envMiddleware } from './middlewares';
 
 export const app = express();
 
 /** middleware */
+app.use(helmet());
 app.use(envMiddleware);
 app.use(async (req, res, next) => {
   await nuxt.ready();
