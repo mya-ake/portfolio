f4bb7d880fbaf9fcbf9c38106150e0d9e4f1c35f
mya-ake <make.app.ts@gmail.com>
2019-08-04 03:01:18 +0900

    chore: Update ci

--- bin/setup_puppeteer.sh
+++ bin/setup_puppeteer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
     && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
     && apt-get update \
     && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst ttf-freefont \
       --no-install-recommends \
     && rm -rf /var/lib/apt/lists/*
 
-RUN yarn add -D puppeteer \
+yarn add -D puppeteer \
     # Add user so we don't need --no-sandbox.
     # same layer as npm install to keep re-chowned files from using up several hundred MBs more space
     && groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
