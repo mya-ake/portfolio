28c9a66de29f42e671ac2651e517ef88f338de04
mya-ake <make.app.ts@gmail.com>
2019-08-04 03:10:28 +0900

    chore: Update ci

--- bin/setup_puppeteer.sh
+++ bin/setup_puppeteer.sh
@@ -8,8 +8,6 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
     && rm -rf /var/lib/apt/lists/*
 
 yarn add -D puppeteer \
-    # Add user so we don't need --no-sandbox.
-    # same layer as npm install to keep re-chowned files from using up several hundred MBs more space
     && groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
     && mkdir -p /home/pptruser/Downloads \
     && chown -R pptruser:pptruser /home/pptruser \
