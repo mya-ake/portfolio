db98d1c0c87976c43e3c21c69ddaf87e75e2a749
mya-ake <make.app.ts@gmail.com>
2021-05-19 23:16:15 +0900

    fix: build scripts

--- scripts/build/index.ts
+++ scripts/build/index.ts
@@ -3,8 +3,13 @@ import { spawn } from '@mya-ake/command-line-utils';
 const singlePackageScriptArgs = ['lerna', 'run', '--stream', '--scope'];
 const serverScriptsArgs = [...singlePackageScriptArgs, '@mya-ake-com/server'];
 const clientScriptsArgs = [...singlePackageScriptArgs, '@mya-ake-com/client'];
+const graphqlSchemaScriptsArgs = [
+  ...singlePackageScriptArgs,
+  '@mya-ake-com/graphql-schema',
+];
 
 const build = async () => {
+  await spawn('yarn', [...graphqlSchemaScriptsArgs, 'build']);
   await spawn('yarn', [...serverScriptsArgs, 'build']);
   const serverPs = await spawn('yarn', [...serverScriptsArgs, 'start'], {
     pararel: true,
