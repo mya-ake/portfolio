3e09bbb4222e9b0086f03ee3e48b5c6e236f74ab
mya-ake <make.app.ts@gmail.com>
2021-05-20 23:30:35 +0900

    feat: create lambda handler

--- packages/server/src/servers/lambda.ts
+++ packages/server/src/servers/lambda.ts
@@ -1,17 +1,14 @@
-import { Express } from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-lambda';
 import { resolvers } from '../resolvers';
 import { getSchema } from '@mya-ake-com/graphql-schema';
 
-export const applyApolloServer = async (
-  app: Express,
-): Promise<ApolloServer> => {
+type Handler = ReturnType<ApolloServer['createHandler']>;
+
+export const createApolloServerHandler = (): Handler => {
   const typeDefs = getSchema();
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const server = new ApolloServer({ typeDefs, resolvers: resolvers as any });
-  await server.start();
 
-  server.applyMiddleware({ app });
-  return server;
+  return server.createHandler();
 };
