5b4a09a1cfb94d06015c11dbe4a5410fda8c2477
mya-ake <make.app.ts@gmail.com>
2021-05-25 23:17:42 +0900

    feat: create post details page

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,26 +1,39 @@
 import Link from 'next/link';
-// import { graphQLSdk } from '~/gateways/graphql';
+import { graphQLSdk } from '~/gateways/graphql';
 import type { NextPage, GetStaticProps } from 'next';
+import type { PostListItemFragment } from '~/graphql';
 
 type Props = {
-  message: string;
+  posts: PostListItemFragment[];
 };
 
-const Home: NextPage<Props> = ({ message }) => {
+const Home: NextPage<Props> = ({ posts }) => {
   return (
     <>
-      <h1>Home</h1>
-      <p>{message}</p>
+      <h1>Posts</h1>
       <div>
         <Link href="/about">to About</Link>
+      </div>
+      <div>
+        {posts.map((post) => (
+          <article key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <a>
+                <h2>{post.title}</h2>
+              </a>
+            </Link>
+          </article>
+        ))}
       </div>
     </>
   );
 };
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
+  const { data } = await graphQLSdk.getPosts();
+  const posts = data?.posts.edges.node ?? [];
   return {
-    props: { message: '' },
+    props: { posts },
     revalidate: 10,
   };
 };
---
+++ packages/client/src/pages/posts/[id]/index.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import { graphQLSdk } from '~/gateways/graphql';
+import type { NextPage, GetStaticProps, GetStaticPaths } from 'next';
+import type { PostDetailsFragment } from '~/graphql';
+
+type Props = {
+  post: PostDetailsFragment;
+};
+
+const Home: NextPage<Props> = ({ post }) => {
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <div>{post.body}</div>
+      <div>
+        <Link href="/">to Home</Link>
+      </div>
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await graphQLSdk.getPosts();
+  const posts = data?.posts.edges.node ?? [];
+
+  const paths = posts.map((post) => ({ params: { id: post.id } }));
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  if (!params || !params.id) {
+    return { notFound: true };
+  }
+  const { data } = await graphQLSdk.getPost({ id: params.id.toString() });
+  if (!data) {
+    return { notFound: true };
+  }
+  const { post } = data;
+  return {
+    props: { post },
+    revalidate: 10,
+    notFound: false,
+  };
+};
+
+export default Home;
