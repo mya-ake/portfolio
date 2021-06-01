b5f6eb43f9b44762527974ee09d660dbcc53517b
mya-ake <make.app.ts@gmail.com>
2021-06-02 00:45:00 +0900

    fix: add local env

--- packages/client/src/shared/env/index.ts
+++ packages/client/src/shared/env/index.ts
@@ -1,5 +1,6 @@
-export type AppEnv = 'dev' | 'prod';
-export const getAppEnv = (): AppEnv => (process.env.APP_ENV as AppEnv) ?? 'dev';
+export type AppEnv = 'dev' | 'prod' | 'local';
+export const getAppEnv = (): AppEnv =>
+  (process.env.APP_ENV as AppEnv) ?? 'local';
 
 export const getAPIEndpoint = (): string =>
   process.env[`API_ENDPOINT_${getAppEnv().toUpperCase()}`] ??
