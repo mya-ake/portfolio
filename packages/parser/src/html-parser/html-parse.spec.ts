f76d8f7f5361986cc12ce1556fa2228b8c1b2a31
mya-ake <make.app.ts@gmail.com>
2021-06-13 08:23:08 +0900

    test: add

--- packages/parser/src/html-parser/html-parse.spec.ts
+++ packages/parser/src/html-parser/html-parse.spec.ts
@@ -1,7 +1,7 @@
 import { parseHtml } from './html-parser';
 
 describe('parseHtml', () => {
-  describe('single child', () => {
+  describe('single root', () => {
     it('no attrs', () => {
       const result = parseHtml(`<p>text</p>`);
       expect(result).toEqual([
@@ -21,7 +21,7 @@ describe('parseHtml', () => {
 
     it('has attrs', () => {
       const result = parseHtml(
-        `<button class="some-class" type="button" disabled>text</button>`,
+        `<button type="button" disabled aria-pressed="false">text</button>`,
       );
       expect(result).toEqual([
         {
@@ -29,16 +29,16 @@ describe('parseHtml', () => {
           tagName: 'button',
           attrs: [
             {
-              name: 'class',
-              value: 'some-class',
-            },
-            {
               name: 'type',
               value: 'button',
             },
             {
               name: 'disabled',
               value: '',
+            },
+            {
+              name: 'aria-pressed',
+              value: 'false',
             },
           ],
           childNodes: [
@@ -49,6 +49,69 @@ describe('parseHtml', () => {
           ],
         },
       ]);
+    });
+  });
+
+  describe('multi root', () => {
+    it('', () => {
+      const result = parseHtml(
+        `<p class="p p1">text 1</p><p class="p">text 2</p>`,
+      );
+      expect(result).toEqual([
+        {
+          nodeType: 'tag',
+          tagName: 'p',
+          attrs: [
+            {
+              name: 'class',
+              value: 'p p1',
+            },
+          ],
+          childNodes: [
+            {
+              nodeType: 'text',
+              content: 'text 1',
+            },
+          ],
+        },
+        {
+          nodeType: 'tag',
+          tagName: 'p',
+          attrs: [
+            {
+              name: 'class',
+              value: 'p',
+            },
+          ],
+          childNodes: [
+            {
+              nodeType: 'text',
+              content: 'text 2',
+            },
+          ],
+        },
+      ]);
+    });
+  });
+
+  describe('deep children', () => {
+    it('list', () => {
+      const result = parseHtml(`<ul><li>item 1</li><li>item 1</li></ul>`);
+      expect(result).toMatchSnapshot();
+    });
+
+    it('some content', () => {
+      const result = parseHtml(
+        `<div>no wrapped<p>item 1</p><img src="/static/sample.png" alt=""/></div>`,
+      );
+      expect(result).toMatchSnapshot();
+    });
+  });
+
+  describe('web components', () => {
+    it('list', () => {
+      const result = parseHtml(`<custom-p>text</custom-p>`);
+      expect(result).toMatchSnapshot();
     });
   });
 });
