060c58bca7c62a2e679b384d05eb6e0662062ecd
mya-ake <make.app.ts@gmail.com>
2021-06-26 08:45:42 +0900

    feat: update layout

--- packages/client/src/components/layout/DefaultLayout/DefaultLayout.tsx
+++ packages/client/src/components/layout/DefaultLayout/DefaultLayout.tsx
@@ -1,9 +1,17 @@
+import { DefaultHeader } from '../DefaultHeader';
+import { DefaultFooter } from '../DefaultFooter';
 import type { FC } from 'react';
 
 export const DefaultLayout: FC = ({ children }) => {
   return (
-    <div className="min-h-screen overflow-auto">
+    <div className="min-h-screen overflow-auto flex flex-col bg-gray-50 text-gray-800">
+      <div>
+        <DefaultHeader />
+      </div>
       <main className="container mx-auto">{children}</main>
+      <div className="mt-auto">
+        <DefaultFooter />
+      </div>
     </div>
   );
 };
