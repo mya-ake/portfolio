36cab2413ee03e70facbaa12e15ee99a230a2512
mya-ake <make.app.ts@gmail.com>
2021-06-28 08:09:28 +0900

    refactor: use grid

--- packages/client/src/components/layout/DefaultLayout/DefaultLayout.tsx
+++ packages/client/src/components/layout/DefaultLayout/DefaultLayout.tsx
@@ -4,14 +4,22 @@ import type { FC } from 'react';
 
 export const DefaultLayout: FC = ({ children }) => {
   return (
-    <div className="min-h-screen overflow-auto flex flex-col bg-gray-50 text-gray-800">
+    <div className="min-h-screen overflow-auto layout bg-gray-50 text-gray-800">
       <div>
         <DefaultHeader />
       </div>
-      <main className="container mx-auto">{children}</main>
+      <main className="container h-full mx-auto">{children}</main>
       <div className="mt-auto">
         <DefaultFooter />
       </div>
+      <style jsx>
+        {`
+          .layout {
+            display: grid;
+            grid-template-rows: auto 1fr auto;
+          }
+        `}
+      </style>
     </div>
   );
 };
