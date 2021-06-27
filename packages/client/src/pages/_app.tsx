c80e27b988336635a0e3c827adc8f9caaa9050fa
mya-ake <make.app.ts@gmail.com>
2021-06-27 09:21:23 +0900

    feat: update fetch

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,9 +1,19 @@
+import { SWRConfig, SWRConfiguration } from 'swr';
 import 'tailwindcss/tailwind.css';
 import type { FC } from 'react';
 import type { AppProps } from 'next/app';
 
+const swrConfig: SWRConfiguration = {
+  revalidateOnFocus: false,
+  revalidateOnReconnect: false,
+};
+
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig value={swrConfig}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 };
 
 export default App;
--- packages/client/src/pages/posts/[id]/index.tsx
+++ packages/client/src/pages/posts/[id]/index.tsx
@@ -1,24 +1,37 @@
-import Link from 'next/link';
+import { useMemo } from 'react';
+import useSWR from 'swr';
 import { graphQLSdk } from '~/gateways/graphql';
-import { parseHtml, Node as HTMLNode } from '@mya-ake-com/parser';
+import { parseHtml } from '@mya-ake-com/parser';
 import { DefaultLayout } from '~/components/layout';
 import { RenderHTML } from '~/components/core/RenderHTML';
 import type { NextPage, GetStaticProps, GetStaticPaths } from 'next';
 import type { PostDetailsFragment } from '~/graphql';
 
+const fetchPost = (id: string) =>
+  graphQLSdk.getPost({ id }).then(({ data }) => data?.post);
+
 type Props = {
   post: PostDetailsFragment;
-  parsedBody: HTMLNode[];
 };
 
-const Home: NextPage<Props> = ({ post, parsedBody = [] }) => {
+const Home: NextPage<Props> = ({ post: propsPost }) => {
+  const { data: post } = useSWR(
+    `/posts/${propsPost.id}`,
+    () => fetchPost(propsPost.id),
+    {
+      initialData: propsPost,
+      revalidateOnMount: true,
+    },
+  );
+
+  const parsedBody = useMemo(() => {
+    return parseHtml(post?.body ?? '');
+  }, [post]);
+
   return (
     <DefaultLayout>
-      <h1>{post.title}</h1>
+      <h1>{post?.title}</h1>
       <RenderHTML htmlNodes={parsedBody} />
-      <div>
-        <Link href="/">to Home</Link>
-      </div>
     </DefaultLayout>
   );
 };
@@ -28,22 +41,23 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const posts = data?.posts.edges.map(({ node }) => node) ?? [];
 
   const paths = posts.map((post) => ({ params: { id: post.id } }));
-  return { paths, fallback: false };
+  return { paths, fallback: 'blocking' };
 };
 
 export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
   if (!params || !params.id) {
     return { notFound: true };
   }
-  const { data } = await graphQLSdk.getPost({ id: params.id.toString() });
+  const { data } = await graphQLSdk.getPost({
+    id: params.id.toString(),
+  });
   if (!data) {
     return { notFound: true };
   }
   const { post } = data;
-  const parsedBody = parseHtml(post.body);
   return {
-    props: { post, parsedBody },
-    revalidate: 10,
+    props: { post },
+    revalidate: 60,
     notFound: false,
   };
 };
