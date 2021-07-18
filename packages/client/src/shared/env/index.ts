8afe7e1cfab04741153890ce542247064b1bca98
mya-ake <make.app.ts@gmail.com>
2021-07-19 01:57:25 +0900

    fix: env

--- packages/client/src/shared/env/index.ts
+++ packages/client/src/shared/env/index.ts
@@ -2,11 +2,24 @@ export type AppEnv = 'dev' | 'prod' | 'local';
 export const getAppEnv = (): AppEnv =>
   (process.env.APP_ENV as AppEnv) ?? 'local';
 
-console.log(process.env);
+export const getAPIEndpoint = (): string => {
+  switch (getAppEnv()) {
+    case 'prod':
+      return process.env.NEXT_PUBLIC_API_ENDPOINT_PROD ?? '';
+    case 'dev':
+      return process.env.NEXT_PUBLIC_API_ENDPOINT_DEV ?? '';
+    default:
+      return 'http://localhost:4000/graphql';
+  }
+};
 
-export const getAPIEndpoint = (): string =>
-  process.env[`NEXT_PUBLIC_API_ENDPOINT_${getAppEnv().toUpperCase()}`] ??
-  'http://localhost:4000/graphql';
-
-export const getAPIKey = (): string =>
-  process.env[`NEXT_PUBLIC_API_KEY_${getAppEnv().toUpperCase()}`] ?? '';
+export const getAPIKey = (): string => {
+  switch (getAppEnv()) {
+    case 'prod':
+      return process.env.NEXT_PUBLIC_API_KEY_PROD ?? '';
+    case 'dev':
+      return process.env.NEXT_PUBLIC_API_KEY_DEV ?? '';
+    default:
+      return '';
+  }
+};
