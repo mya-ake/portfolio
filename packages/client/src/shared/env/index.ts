6e5c480984d45a496d287fdd21e275d5de511f94
mya-ake <make.app.ts@gmail.com>
2021-07-19 01:40:18 +0900

    chore: debug

--- packages/client/src/shared/env/index.ts
+++ packages/client/src/shared/env/index.ts
@@ -2,6 +2,8 @@ export type AppEnv = 'dev' | 'prod' | 'local';
 export const getAppEnv = (): AppEnv =>
   (process.env.APP_ENV as AppEnv) ?? 'local';
 
+console.log(process.env);
+
 export const getAPIEndpoint = (): string =>
   process.env[`NEXT_PUBLIC_API_ENDPOINT_${getAppEnv().toUpperCase()}`] ??
   'http://localhost:4000/graphql';
