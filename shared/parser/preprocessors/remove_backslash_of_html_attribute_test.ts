70a4ac238f86291387ad662231efd4dab5d05fec
mya-ake <make.app.ts@gmail.com>
2022-07-15 21:01:34 +0900

    fix: format

--- shared/parser/preprocessors/remove_backslash_of_html_attribute_test.ts
+++ shared/parser/preprocessors/remove_backslash_of_html_attribute_test.ts
@@ -5,11 +5,11 @@ Deno.test({
   name: "remove backslash from attributes",
   fn() {
     const result = removeBackslashOfHTMLAttribute(
-      `<a href=\\"https://example.com\\" rel=\\"noopener noreferrer\\">https://example.com</a>`
+      `<a href=\\"https://example.com\\" rel=\\"noopener noreferrer\\">https://example.com</a>`,
     );
     assertEquals(
       result,
-      `<a href="https://example.com" rel="noopener noreferrer">https://example.com</a>`
+      `<a href="https://example.com" rel="noopener noreferrer">https://example.com</a>`,
     );
   },
 });
@@ -18,11 +18,11 @@ Deno.test({
   name: "don't remove from a content",
   fn() {
     const result = removeBackslashOfHTMLAttribute(
-      `<a href=\\"https://example.com\\" rel=\\"noopener noreferrer\\">\\</a>`
+      `<a href=\\"https://example.com\\" rel=\\"noopener noreferrer\\">\\</a>`,
     );
     assertEquals(
       result,
-      `<a href="https://example.com" rel="noopener noreferrer">\\</a>`
+      `<a href="https://example.com" rel="noopener noreferrer">\\</a>`,
     );
   },
 });
