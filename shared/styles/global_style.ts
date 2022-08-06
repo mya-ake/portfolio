5043e51c7bbea0eb40b583fc9cf2cd17b0cf89d7
mya-ake <myaaake@gmail.com>
2022-08-06 23:18:38 +0900

    feat: reset heading

--- shared/styles/global_style.ts
+++ shared/styles/global_style.ts
@@ -7,4 +7,34 @@ export const globalStyles = globalCss({
     "-webkit-font-smoothing": "antialiased",
     "-moz-osx-font-smoothing": "grayscale",
   },
+  h1: {
+    margin: 0,
+    fontSize: "1rem",
+    lineHeight: "1",
+  },
+  h2: {
+    margin: 0,
+    fontSize: "1rem",
+    lineHeight: "1",
+  },
+  h3: {
+    margin: 0,
+    fontSize: "1rem",
+    lineHeight: "1",
+  },
+  h4: {
+    margin: 0,
+    fontSize: "1rem",
+    lineHeight: "1",
+  },
+  h5: {
+    margin: 0,
+    fontSize: "1rem",
+    lineHeight: "1",
+  },
+  h6: {
+    margin: 0,
+    fontSize: "1rem",
+    lineHeight: "1",
+  },
 });
