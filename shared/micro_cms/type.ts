ffe2a52954bab19821162f91c019fb77d130380e
mya-ake <myaaake@gmail.com>
2023-01-24 22:46:04 +0900

    feat: add Rich Editor types

--- shared/micro_cms/type.ts
+++ shared/micro_cms/type.ts
@@ -8,6 +8,7 @@ export type ContentMetadata = {
 
 export type CustomDataValue =
   | string
+  | RichEditor
   | Content<
     Record<string, string>
   >
@@ -41,8 +42,81 @@ export type Tag = Content<TagData>;
 type PostData = {
   title: string;
   description: string;
-  body: string;
+  body: RichEditor;
   tags: Content<TagData>[];
 };
 
 export type Post = Content<PostData>;
+
+/** Rich Editor Object */
+export type RichEditorItemText = {
+  type: "text";
+  value: string;
+  attributes: {
+    italic?: true;
+    bold?: true;
+    underline?: true;
+    strike?: true;
+    code?: true;
+    size?: "small" | "large" | "huge";
+    background?: string;
+  };
+};
+export type RichEditorItemLinkText = {
+  type: "text";
+  value: string;
+  attributes: {
+    link: string;
+    target?: "_blank";
+    rel?: string;
+  };
+};
+export type RichEditorItemBlock = {
+  type: "block";
+  value: (RichEditorItemText | RichEditorItemLinkText | RichEditorItemImage)[];
+  attributes: {
+    header?: 1 | 2 | 3 | 4 | 5;
+    align?: "center" | "right" | "justify";
+    blockquote?: true;
+  };
+};
+export type RichEditorItemListBlock = {
+  type: "block";
+  value: RichEditorItemText[];
+  attributes: {
+    list: "ordered" | "bullet";
+    indent: number;
+  };
+};
+export type RichEditorItemCodeBlock = {
+  type: "block";
+  value: RichEditorItemText;
+  attributes: {
+    "code-block": true;
+  };
+};
+export type RichEditorItemTextBlock = {
+  type: "textBlock";
+  value: (RichEditorItemText | RichEditorItemLinkText)[];
+};
+export type RichEditorItemImage = {
+  type: "image";
+  value: string;
+  attributes: {
+    link: string | null;
+    target: "_blank" | null;
+    alt: string;
+    width: string;
+    height: string;
+  };
+};
+export type RichEditorRoot =
+  | RichEditorItemBlock
+  | RichEditorItemListBlock
+  | RichEditorItemCodeBlock
+  | RichEditorItemTextBlock
+  | RichEditorItemImage;
+
+export type RichEditor = {
+  contents: RichEditorRoot[];
+};
