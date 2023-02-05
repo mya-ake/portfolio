088903416f56e7a8631fea7e80d2a552d6fb5a4a
mya-ake <myaaake@gmail.com>
2023-02-05 16:34:13 +0900

    feat: update list style

--- shared/ui/list/OrderList.tsx
+++ shared/ui/list/OrderList.tsx
@@ -1,16 +1,9 @@
 /** @jsx h */
 import { ComponentChildren, h } from "preact";
-import { css } from "@shared/styles/css.ts";
+import { baseListStyle } from "./_style.ts";
 
-const style = css({
-  margin: 0,
-  listStylePosition: "outside",
-});
-
-type ListStyleType = "disc" | "none";
 type Props = {
   children: ComponentChildren;
-  listStyleType?: ListStyleType;
 };
 
 export function OrderList(props: Props) {
@@ -18,11 +11,8 @@ export function OrderList(props: Props) {
 
   return (
     <ol
-      class={style({
-        css: {
-          display: "grid",
-          rowGap: "$2",
-        },
+      class={baseListStyle({
+        padding: "0 0 0 $4",
       })}
     >
       {children}
