92ab9b26506debdac87613cecdfb7bb1dc08a6b2
mya-ake <myaaake@gmail.com>
2023-03-18 11:23:32 +0900

    fix: path name

--- shared/cache/local/instant_cache_test.ts
+++ shared/cache/local/instant_cache_test.ts
@@ -1,6 +1,6 @@
 import { afterEach } from "std/testing/bdd.ts";
 import { join } from "std/path/mod.ts";
-import { testBasicSpec } from "../_instant_cache_test_cast.ts";
+import { testBasicSpec } from "../_instant_cache_test_case.ts";
 import { createInstantCache } from "./instant_cache.ts";
 
 const tmpDir = join(Deno.cwd(), ".tmp/cache");
--- shared/cache/memory/instant_cache_test.ts
+++ shared/cache/memory/instant_cache_test.ts
@@ -1,4 +1,4 @@
-import { testBasicSpec } from "../_instant_cache_test_cast.ts";
+import { testBasicSpec } from "../_instant_cache_test_case.ts";
 import { createInstantCache } from "./instant_cache.ts";
 
 testBasicSpec(createInstantCache);
