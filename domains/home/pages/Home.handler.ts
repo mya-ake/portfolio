f523407c4295ad35a4ba89191afcfe43a6209170
mya-ake <myaaake@gmail.com>
2023-03-19 09:36:33 +0900

    feat: use cms

--- domains/home/pages/Home.handler.ts
+++ domains/home/pages/Home.handler.ts
@@ -5,9 +5,13 @@ import {
   Repository,
 } from "@shared/github/mod.ts";
 import { cacheMiddleware } from "@shared/middleware/cache.ts";
+import { getWidgets, WidgetMap } from "@shared/widget/mod.ts";
 
+const widgetIds = ["home_about", "home_recent_activities"] as const;
+type WidgetId = typeof widgetIds[number];
 export type Data = {
   repositories: Repository[];
+  widgetMap: WidgetMap<WidgetId>;
 };
 
 function getRepositories() {
@@ -18,7 +22,11 @@ function getRepositories() {
 export const handler: Handlers<Data> = {
   async GET(_, ctx) {
     const repositories = await getRepositories();
-    const data: Data = { repositories };
+    const widgetMap = await getWidgets<WidgetId>(widgetIds);
+    const data: Data = {
+      repositories,
+      widgetMap,
+    };
 
     const resp = await ctx.render(data);
     cacheMiddleware(resp, { time: 60 * 60 * 24 * 7 });
