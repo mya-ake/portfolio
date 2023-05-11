dbd520029b34b267b03e774b05dc96325148e33d
mya-ake <myaaake@gmail.com>
2023-05-11 11:28:37 +0900

    chore: update

--- shared/date/_dayjs.ts
+++ shared/date/_dayjs.ts
@@ -2,23 +2,6 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 
-// Can't read the type, so I copied it.
-declare module "dayjs" {
-  interface Dayjs {
-    tz(timezone?: string, keepLocalTime?: boolean): Dayjs;
-    offsetName(type?: "short" | "long"): string | undefined;
-  }
-
-  interface DayjsTimezone {
-    (date: ConfigType, timezone?: string): Dayjs;
-    (date: ConfigType, format: string, timezone?: string): Dayjs;
-    guess(): string;
-    setDefault(timezone?: string): void;
-  }
-
-  const tz: DayjsTimezone;
-}
-
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
