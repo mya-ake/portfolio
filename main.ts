7b80abd8b565a1e679b96bfe83fd785188f5f768
mya-ake <myaaake@gmail.com>
2023-05-11 10:49:41 +0900

    fix: hostname (#1138)

--- main.ts
+++ main.ts
@@ -7,14 +7,16 @@
 import {
   InnerRenderFunction,
   RenderContext,
-  start,
+  ServerContext,
   StartOptions,
 } from "$fresh/server.ts";
+import { serve, Status } from "$fresh/src/server/deps.ts";
 import manifest from "./fresh.gen.ts";
 import { readNormalizeCss } from "./core/css/mod.ts";
 import { getCssText, reset } from "@shared/styles/core.ts";
 import { globalStyles } from "@shared/styles/global_styles.ts";
 import { getCurrentLang } from "@shared/i18n/mod.ts";
+import type { Handler } from "std/http/server.ts";
 
 async function render(ctx: RenderContext, render: InnerRenderFunction) {
   // lang
@@ -31,8 +33,27 @@ async function render(ctx: RenderContext, render: InnerRenderFunction) {
   ctx.styles.push(getCssText());
 }
 
-const options: StartOptions = { render, port: 8000 };
-if (Deno.env.get("APP_ENV") === "prod") {
-  options.hostname = "mya-ake.com";
+async function start() {
+  const options: StartOptions = { render, port: 8000 };
+  const ctx = await ServerContext.fromManifest(manifest, options);
+
+  const handler: Handler = (req, connInfo) => {
+    // Emergency response to redirect to a different host.
+    const url = new URL(req.url);
+    if (url.pathname.length > 1 && url.pathname.endsWith("/")) {
+      url.pathname = url.pathname.slice(0, -1);
+      return new Response(undefined, {
+        status: Status.TemporaryRedirect,
+        headers: {
+          location: url.pathname,
+        },
+      });
+    }
+
+    return ctx.handler()(req, connInfo);
+  };
+
+  await serve(handler, options);
 }
-await start(manifest, options);
+
+await start();
