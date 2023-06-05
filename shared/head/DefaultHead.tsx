6441fb05a08c8c8e86f7ac58b32280bfe27e85db
mya-ake <myaaake@gmail.com>
2023-06-06 08:28:28 +0900

    fix: script position (#1155)

--- shared/head/DefaultHead.tsx
+++ shared/head/DefaultHead.tsx
@@ -1,9 +1,6 @@
 import { Head } from "$fresh/runtime.ts";
-import { getGATagId } from "@shared/env/mod.ts";
 
 export function DefaultHead() {
-  const gaTagId = getGATagId();
-
   return (
     <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -16,13 +13,6 @@ export function DefaultHead() {
         href="https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap"
         rel="stylesheet"
       />
-      {gaTagId && (
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${gaTagId}`}
-        >
-        </script>
-      )}
     </Head>
   );
 }
