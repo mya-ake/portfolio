5658697e7dfe12f0f7457a458d3db46fcb33d510
mya-ake <myaaake@gmail.com>
2023-06-08 11:22:51 +0900

    feat: add cache

--- routes/sitemap.ts
+++ routes/sitemap.ts
@@ -1,14 +1,18 @@
 import { Handlers } from "$fresh/server.ts";
 import { createSitemap } from "@sitemap/mod.ts";
+import { cacheMiddleware } from "@shared/middleware/cache.ts";
 
 export const handler: Handlers = {
   async GET() {
     const sitemap = await createSitemap();
-    return new Response(sitemap, {
+
+    const resp = new Response(sitemap, {
       status: 200,
       headers: {
         "content-type": "application/xml",
       },
     });
+    cacheMiddleware(resp, { time: 60 * 60 * 12 });
+    return resp;
   },
 };
