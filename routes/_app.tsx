95bcb2b525ca0898cf9d0f7d5aeb1b33a8231287
mya-ake <myaaake@gmail.com>
2023-09-11 09:47:00 +0900

    feat: add ad (#1168)

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,10 +2,11 @@ import { AppProps } from "$fresh/server.ts";
 import { DefaultHead } from "@shared/head/DefaultHead.tsx";
 import { Grid } from "@shared/ui/layout/Grid.tsx";
 import Gtag from "@islands/Gtag.tsx";
-import { getGATagId } from "@shared/env/mod.ts";
+import { getGAdId, getGATagId } from "@shared/env/mod.ts";
 
 export default function App({ Component }: AppProps) {
   const gaTagId = getGATagId();
+  const gAdId = getGAdId();
 
   return (
     <>
@@ -13,6 +14,14 @@ export default function App({ Component }: AppProps) {
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${gaTagId}`}
+        >
+        </script>
+      )}
+      {gAdId && (
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${gAdId}`}
+          crossOrigin="anonymous"
         >
         </script>
       )}
